f5f66526d3042733bd80986046490e86f2ec1e7d
Lance Page <6ftdotexe@users.noreply.github.com>
2020-08-16 19:46:54 -0500

    building out components and pages

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+function Form(props) {
+    return (
+        <form>
+            <div className="form-group">
+                <label htmlFor="search">Book Search:</label>
+                <input
+                    onChange={props.handleInputChange}
+                    value={props.search}
+                    name="search"
+                    type="text"
+                    className="form-control"
+                    placeholder="Search for a Book"
+                    id="search"
+                />
+                <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">Search</button>
+            </div>
+        </form>
+    );
+}
+
+export default Form;
---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { link, Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">Google Books</Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link
+                            to="/search"
+                            className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}>
+                            Search
+                        </Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link
+                            to="/saved"
+                            className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}>
+                            Saved
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
